0a93999655dad1a7b30d6a9387a6547f50bad8fa
Kylesk22 <Kylesk22@gmail.com>
2022-07-22 19:54:34 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -6,5 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "his", "her", "their"];
+  let adj = ["great", "awesome", "terrible", "horrifying", "funny"];
+  let noun = ["dog", "cat", "mailman", "surfer", "flask"];
+  let ext = [".com", ".org,", ".net", ".io", ".us"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    let a = pronoun[i];
+    for (let i = 0; i < adj.length; i++) {
+      let b = adj[i];
+      for (let i = 0; i < noun.length; i++) {
+        let c = noun[i];
+        for (let i = 0; i < ext.length; i++) {
+          let d = ext[i];
+          console.log(a + b + c + d);
+          document.getElementById("generated-names").innerHTML +=
+            a + b + c + d + "\n";
+        }
+      }
+    }
+  }
 };
